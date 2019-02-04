76c916c19dae51c84fef6c3d52987b09f06313df
Yong Xia <xywork008@gmail.com>
2019-02-04 16:41:57 -0600

    update components to stateless

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ class App extends React.Component {
     if (city && country) {
       const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
       const data = await api_call.json();
-      console.log(data);
       this.setState({
         temperature: data.main.temp,
         city: data.name,
