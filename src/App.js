c998f3fe799155f64c2c0216c16f886c2f9b2ce3
Yong Xia <xywork008@gmail.com>
2019-02-04 18:54:06 -0600

    add style for weather app

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     if (city && country) {
       const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
       const data = await api_call.json();
+      console.log(data);
       this.setState({
         temperature: data.main.temp,
         city: data.name,
@@ -47,20 +48,32 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Titles />
-        <Form getWeather={this.getWeather} />
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
-        />
+      <div className="wrapper">
+        <div className="main">
+          <div className="row">
+            <div className="col-xs-5 title-container">
+              <Titles />
+            </div>
+            <div className="col-xs-7 form-container">
+              <Form getWeather={this.getWeather} />
+              <Weather
+                temperature={this.state.temperature}
+                humidity={this.state.humidity}
+                city={this.state.city}
+                country={this.state.country}
+                description={this.state.description}
+                error={this.state.error}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
